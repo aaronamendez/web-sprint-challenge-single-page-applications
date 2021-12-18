d64462726ee42d77673e57ac0dcb541c05e2e7bd
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2021-12-17 19:54:22 -0500

    Cleaned up some code.

--- src/components/OrderConfirmed/OrderConfirmed.js
+++ src/components/OrderConfirmed/OrderConfirmed.js
@@ -2,28 +2,8 @@ import React from 'react';
 import './index.css';
 
 const OrderConfirmed = ({ data }) => {
-	// const error = () => {
-	// 	<div>
-	// 		<h2>404 ERROR!</h2>
-	// 	</div>;
-	// };
-
-	// const message = () => {
-	// 	<div>
-	// 		<h2>Great!</h2>
-	// 	</div>;
-	// };
-
-	// const dataExists = (data) => {
-	// 	if (data === !null) {
-	// 		return message;
-	// 	} else {
-	// 		return error;
-	// 	}
-	// };
 	return (
 		<div className="confirmation">
-			{/* {dataExists(data) ? message : error} */}
 			<h2>Congrats! Pizza is on it's way!</h2>
 			<div>
 				<p>Name: {data.name}</p>
